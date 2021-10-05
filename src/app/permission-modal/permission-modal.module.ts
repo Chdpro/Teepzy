607faedf6aee1413066ffaa2945209e4d9533166
Chrissmal <chrishouns21@gmail.com>
2021-10-05 00:02:42 -0700

    merge

--- src/app/permission-modal/permission-modal.module.ts
+++ src/app/permission-modal/permission-modal.module.ts
@@ -7,6 +7,13 @@ import { IonicModule } from "@ionic/angular";
 import { PermissionModalPageRoutingModule } from "./permission-modal-routing.module";
 
 import { PermissionModalPage } from "./permission-modal.page";
+import { HttpClient } from "@angular/common/http";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+// Factory function required during AOT compilation
+export function httpTranslateLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   imports: [
@@ -14,6 +21,13 @@ import { PermissionModalPage } from "./permission-modal.page";
     FormsModule,
     IonicModule,
     PermissionModalPageRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   entryComponents: [PermissionModalPage],
   declarations: [PermissionModalPage],
